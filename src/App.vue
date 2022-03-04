3beeefb5ef73e5f3d498b6c7f9e52ef62ce355e6
Angela De Pasquale <angeladepasquale_@libero.it>
2022-03-04 16:07:23 +0100

    Scheletro parte header

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <MyHeader />
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/style/general.scss";
+@import '~@fortawesome/fontawesome-free/css/all.css'
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="sec-header">
+        <div class="header-top">
+            <div class="social-sec">
+                <ul class="social-list">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="account-sec">
+                <ul class="account-list">
+                    <li>
+                        <a href="#">
+                            Shopping Cart
+                        </a>
+                    </li>
+                    <li class="account-element">
+                        <a href="#">
+                            My Account
+                            <i class="fa-solid fa-angle-down"></i>
+                        </a>
+                    </li>
+                    <li class="cart-element">
+                        <a href="#">
+                            CART
+                        </a>    
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="header-bottom">
+            <div class="logo-sec">
+                <img src="../assets/img/classic_shop_logo1x.png" alt="">
+            </div>
+            <div class="right-header-sec">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Home
+                                <i class="fa-solid fa-angle-down"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shopping
+                                <i class="fa-solid fa-angle-down"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Products
+                                <i class="fa-solid fa-angle-down"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Categories
+                                <i class="fa-solid fa-angle-down"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                News
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Elements
+                                <i class="fa-solid fa-angle-down"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <button class="shop-now">SHOP NOW!</button>
+                <span class="search-icon">
+                    <a href="#">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </a>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader'
+}
+</script>
+
+<style scoped>
+@import "../assets/style/myheader.scss";
+</style>
