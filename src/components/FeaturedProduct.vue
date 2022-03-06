babd5a57b825a55c95e6bc5d77c4fa0ede9877f8
Angela De Pasquale <angeladepasquale_@libero.it>
2022-03-06 15:45:55 +0100

    Scheletro Featured Products

--- src/components/FeaturedProduct.vue
+++ src/components/FeaturedProduct.vue
@@ -19,6 +19,53 @@
                 </li>
             </ul>
         </div>
+        <div class="ft-products-content">
+            <div class="ft-product-card">
+                <div class="ft-product-card-img">
+                    <img src="../assets/img/black_elegant_leather_jacket.jpg" alt="">
+                </div>
+                <div class="ft-product-card-text">
+                    <h1>Black Leather Jacket</h1>
+                    <div class="tags">Men, Jackets, Jeans</div>
+                    <br>
+                    <span class="original-price">$235</span>
+                    <span>$200</span>
+                </div> 
+            </div>
+            <div class="ft-product-card">
+                <div class="ft-product-card-img">
+                    <img src="../assets/img/black_leather_suit.jpg" alt="">
+                </div>
+                <div class="ft-product-card-text">
+                    <h1>Black Leather Suit</h1>
+                    <div class="tags">Men, Jackets</div>
+                    <br>
+                    <span>$176</span>
+                </div> 
+            </div>
+            <div class="ft-product-card">
+                <div class="ft-product-card-img">
+                    <img src="../assets/img/blue_jacket_and_white_stripe_tee.jpg" alt="">
+                </div>
+                <div class="ft-product-card-text">
+                    <h1>Blue Jacket & Striped Tee</h1>
+                    <div class="tags">Men, Jackets, Suits</div>
+                    <br>
+                    <span>$580</span>
+                </div> 
+            </div>
+            <div class="ft-product-card">
+                <div class="ft-product-card-img">
+                    <img src="../assets/img/blue_leather_jacket.jpg" alt="">
+                </div>
+                <div class="ft-product-card-text">
+                    <h1>Blue Leather Jacket</h1>
+                    <div class="tags">Men, Jackets</div>
+                    <br>
+                    <span>$96</span>
+                </div> 
+            </div>
+        </div>
     </div>
 </template>
 
