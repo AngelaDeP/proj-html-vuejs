56c677640bbe9c956623da3329751067215de260
Angela De Pasquale <angeladepasquale_@libero.it>
2022-03-07 12:54:13 +0100

    Risolto il problema immagini Blog

--- src/components/FromOurBlog.vue
+++ src/components/FromOurBlog.vue
@@ -8,7 +8,7 @@
         </div>
         <div class="blog-content">
             <div class="blog-column" v-for="(content, index) in contents" :key="index">
-                <img :src="content.url">
+                <img :src="require(`../assets/img/` + content.url)">
                 <div class="title">{{content.title}}</div>
                 <span class="date">{{content.date}}</span> | <span class="comments">{{content.comments}}</span>
                 <p class="snippet">{{content.snippet}}</p>
@@ -24,21 +24,21 @@ export default {
     return {
       contents: [
           {
-              url: '../assets/img/post_img_12-320x202.jpg',
+              url: 'post_img_12-320x202.jpg',
               title: "Aenean lobortis sapienenim viverra",
               date: "September 9th, 2015",
               comments: "0 Comments",
               snippet: "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla."
           },
           {
-              url: '../assets/img/post_img_11-320x202.jpg',
+              url: 'post_img_11-320x202.jpg',
               title: "Aenean lobortis sapienenim viverra",
               date: "September 9th, 2015",
               comments: "0 Comments",
               snippet: "Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla."
           },
           {
-              url: '../assets/img/post_img_10-320x202.jpg',
+              url: 'post_img_10-320x202.jpg',
               title: "Aenean lobortis sapienenim viverra",
               date: "September 9th, 2015",
               comments: "0 Comments",
